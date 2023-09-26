97147219e930f414f8b75efdd756fd3ea44801a3
MMate23 <132448311+MMate23@users.noreply.github.com>
2023-09-26 13:58:55 +0200

    Start main and jokes by type page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,31 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+
 
 function App() {
 
+  const [jokes, setJokes] = useState([])
+
+  useEffect(() => {
+    const fetchJokes = async () => {
+      const response = await fetch('/api/jokes');
+      const data = await response.json();
+      console.log(data);
+      setJokes(data)
+      if (response.status === 200) {
+        console.log("List is here");
+      }
+    }
+    fetchJokes()
+  }, [])
+
+ console.log(jokes);
+
 
   return (
-    <>
-     
-    </>
+    <div>
+      Main page
+    </div>
   )
 }
 
