0048b90fcfa6cd23c539b78b6aa18abc8e4f5083
MMate23 <132448311+MMate23@users.noreply.github.com>
2023-09-27 09:37:36 +0200

    Implement jokes by type component and the main contains their path

--- client/src/Joke.jsx
+++ client/src/Joke.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react"
 
-export default function Joke({Joke}){
+export default function Joke({ joke }) {
 
-    return(
-        <div>
-            <div>{Joke.setup}</div>
-            <h3>{Joke.punchline}</h3>
-        </div>
+    const [punchVisible, setPunchVisible] = useState(false)
+
+    return (
+        <>
+            <div className="oneJoke">
+                <h2>{joke.setup}</h2>
+                {punchVisible ? <h3 style={{ color: 'red' }}>{joke.punchline}</h3> : <h3>Funny content</h3>}
+                <button onClick={() => setPunchVisible(!punchVisible)}>{punchVisible ? 'Bezárás' : 'Nevetésért katt'}</button>
+                <button style={{ color: 'green' }}>Like</button>
+                <button style={{ color: 'red' }}>Dislike</button>
+            </div>
+        </>
     )
 }
+
