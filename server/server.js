84d9215eb2e52f7216918e5ebb98ae4f2d9e2576
Panczel Peter <80787160+PePa82@users.noreply.github.com>
2023-09-27 10:53:33 +0200

    Create user endpoint

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from "express";
 import mongoose from 'mongoose';
 import {Joke as JokeModel} from "./db/jokes.model.js";
+import User from './db/userData.js';
 
 const { MONGO_URL, PORT = 3000 } = process.env;
 
@@ -32,6 +33,26 @@ app.get('/api/jokes', async (req,res)=>{
     res.send(jokeList).status(200)
 })
 
+//user endpoint
+app.post('/api/user', async (req, res) => {
+
+  const name = req.body.name;
+  const email = req.body.email;
+  const createdAt = Date.now();
+
+  const user = new User({
+    name,
+    email,
+    createdAt
+  });
+  try {
+    await user.save();
+    res.status(201).json({success: true});
+  } catch (error) {
+    console.error(error);
+    res.json({success: false})
+  }
+});
 
 
 // Type endpoints before this line
