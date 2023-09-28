7f83c882fbeb33dd05b2d74e4d1b0d3ef7deb9af
Intcognito <130778504+Intcognito@users.noreply.github.com>
2023-09-28 13:27:07 +0200

    fixed box position in css

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-import User from "./components/User.jsx";
 import App from './App.jsx';
 import OwnJokes from './components/OwnJokes.jsx';
 import JokesByType from './JokesByType';
