f49d4d70bdff9b50df7c7f5da8882a458e2787c7
Panczel Peter <80787160+PePa82@users.noreply.github.com>
2023-09-28 17:14:52 +0200

    Update project

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,6 @@ import "./Header.css"
 
 export default function Header() {
   const location = useLocation();
-  console.log(location)
 
   const user = localStorage.getItem("user");
 
@@ -17,18 +16,13 @@ export default function Header() {
       <button className='home-button'><Link to="/home">Home</Link></button>
       <button className='random-joke-button'><Link to="/random">Random Joke</Link></button>
       {!user ? 
-<<<<<<< HEAD
-      <button><Link to="/login">Log In</Link></button> :
-      <button onClick={handleClick}><Link to="/home">{user}</Link></button>}
-=======
-      <button className='login-button'><Link to="/home">Log In</Link></button> :
+      <button className='login-button'><Link to="/login">Log In</Link></button> :
       <div className='user-buttons'>
       <button className='myjokes-button'><Link to="/ownJokes">My okes</Link></button>
-      <button className='profile'><Link to="/ownJokes">Profile</Link></button>
+      <button className='profile'><Link to="/profile">Profile</Link></button>
       <button className='logout-button' onClick={handleLogOut}><Link to="/home">Log Out</Link></button>
       </div>
       }
->>>>>>> development
     </div>
     <Outlet/>
     </>
