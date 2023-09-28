a46832394f5712731f5f7bf112fb02c0f81ff5ad
Marcell Biro <115219769+Marcel-zb96@users.noreply.github.com>
2023-09-28 14:29:05 +0200

    style header fix

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -13,19 +13,21 @@ export default function Header() {
 
   return (
     <>
-    <div className='header-box'>
-      <button className='home-button'><Link to="/home">Home</Link></button>
-      <button className='random-joke-button'><Link to="/random">Random Joke</Link></button>
-      {!user ? 
-      <button className='login-button'><Link to="/home">Log In</Link></button> :
-      <div className='user-buttons'>
-      <button className='myjokes-button'><Link to="/ownJokes">My okes</Link></button>
-      <button className='profile'><Link to="/ownJokes">Profile</Link></button>
-      <button className='logout-button' onClick={handleLogOut}><Link to="/home">Log Out</Link></button>
+      <div className='header-box'>
+        <button className='home-button'><Link to="/home">Home</Link></button>
+        <button className='random-joke-button'><Link to="/random">Random Joke</Link></button>
+        {!user
+          ? <button className='login-button'><Link to="/home">Log In</Link></button>
+          : (
+            <div className='user-buttons'>
+              <button className='myjokes-button'><Link to="/ownJokes">My okes</Link></button>
+              <button className='profile'><Link to="/ownJokes">Profile</Link></button>
+              <button className='logout-button' onClick={handleLogOut}><Link to="/home">Log Out</Link></button>
+            </div>
+            )
+        }
       </div>
-      }
-    </div>
-    <Outlet/>
+      <Outlet />
     </>
   )
 }
