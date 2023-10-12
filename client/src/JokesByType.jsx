90eb6aabad23b39f9f76531d6566a92472cba405
MMate23 <132448311+MMate23@users.noreply.github.com>
2023-10-12 13:40:36 +0200

    Implement sort by number of likes

--- client/src/JokesByType.jsx
+++ client/src/JokesByType.jsx
@@ -5,21 +5,24 @@ import { useEffect, useState } from "react"
 export default function JokesByType({ jokeType }) {
 
   const [jokes, setJokes] = useState([])
+  const [sortByLike, setSortByLike] = useState(false)
+
 
   useEffect(() => {
     const fetchJokes = async () => {
-      const response = await fetch(`/api/jokes?type=${jokeType}`);
+      const response = await fetch(`/api/jokes?type=${jokeType}&sort=${sortByLike ? "asc" : "desc"}`);
       const data = await response.json();
       setJokes(data)
     }
     fetchJokes()
-  }, [])
+  }, [sortByLike])
 
-    return (
-        <>
-            <ul>
-                {jokes.map((joke) => <Joke joke={joke} />)}
-            </ul>
-        </>
-    )
+  return (
+    <>
+      <button onClick={() => setSortByLike(!sortByLike)}>Sort By Likes</button>
+      <ul>
+        {jokes.map((joke) => <Joke key={joke._id} joke={joke} />)}
+      </ul>
+    </>
+  )
 }
